0ae06a55cae90723222d7dce4767184209a0272a
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-05-04 16:32:35 -0700

    Header File

--- sort.h
+++ sort.h
@@ -32,6 +32,8 @@ void quick_sort(int *array, size_t size);
 int partition(int *array, int start, int end, size_t size);
 void quicker(int *array, int start, int end, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap_nodes(listint_t *current_node, listint_t *old_node, listint_t **mylist);
 
 
 #endif
