a69df38a73b33902f53ee06d112127dab8e7eac7
Mahmoud-Rehan <mahmoudrehan38@gmail.com>
2024-05-04 09:31:55 -0700

    Header File

--- sort.h
+++ sort.h
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 
 /**
@@ -30,6 +31,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int partition(int *array, int start, int end, size_t size);
 void quicker(int *array, int start, int end, size_t size);
+void shell_sort(int *array, size_t size);
 
 
 #endif
